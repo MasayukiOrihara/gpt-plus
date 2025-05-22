df898d5174cb8316b69fcb5f223284f8769fa304
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-05-22 16:01:22 +0900

    jsonからプロンプトテンプレートが読みだせないのでべた書きに修正

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,4 @@
-import { PromptTemplateJson } from '@/contents/type';
 import { ChatOpenAI } from '@langchain/openai';
-import {isObject, loadJsonFile } from '@/contents/utils';
 import { PromptTemplate } from '@langchain/core/prompts';
 import { Message as VercelChatMessage, LangChainAdapter } from 'ai';
  
@@ -8,6 +6,8 @@ import { Message as VercelChatMessage, LangChainAdapter } from 'ai';
 const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
+
+const PROMPT_TEMPLATE_TEXT = `"Current conversation:\n{history}\n\nuser:{input}\nAI:`;
 
 /**
  * チャット応答AI
@@ -27,21 +27,6 @@ export async function POST(req: Request) {
     // メッセージ{input}
     const currentMessageContent = messages[messages.length - 1].content;
 
-    //プロンプトテンプレートの作成
-    const template = await loadJsonFile<PromptTemplateJson[]>('data/prompt-template.json');
-    if (!template.success) {
-      return new Response(JSON.stringify({ error: template.error }),{
-        status: 500,
-        headers: { 'Content-type' : 'application/json' },
-      });
-    }
-
-    // プロンプトテンプレートの抽出
-    const found = template.data.find(obj => isObject(obj) && obj['name'] === 'api-langchain-memory');
-    if (!found) {
-      throw new Error('テンプレートが見つかりませんでした');
-    }
-
     // モデルの指定
     const model = new ChatOpenAI({
       apiKey: process.env.OPENAI_API_KEY!,
@@ -49,7 +34,7 @@ export async function POST(req: Request) {
       temperature: 0.8,
       });
         
-    const prompt = PromptTemplate.fromTemplate(found.template);
+    const prompt = PromptTemplate.fromTemplate(PROMPT_TEMPLATE_TEXT);
     const chain = prompt.pipe(model);
 
     const stream = await chain.stream({
