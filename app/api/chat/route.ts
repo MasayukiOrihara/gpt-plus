6f57cf3132d6eb78e5d1384fc370c199355e939d
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-05-23 10:53:40 +0900

    プロンプトをLangsmithに変更

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,44 +1,48 @@
-import { ChatOpenAI } from '@langchain/openai';
-import { PromptTemplate } from '@langchain/core/prompts';
-import { Message as VercelChatMessage, LangChainAdapter } from 'ai';
- 
-// チャット形式
-const formatMessage = (message: VercelChatMessage) => {
-  return `${message.role}: ${message.content}`;
-};
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { Message as VercelChatMessage, LangChainAdapter } from "ai";
+import { Client } from "langsmith";
 
-const PROMPT_TEMPLATE_TEXT = `"Current conversation:\n{history}\n\nuser:{input}\nAI:`;
+const client = new Client({
+  apiKey: process.env.LANGSMITH_API_KEY,
+});
+
+const model = new ChatOpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+  model: "gpt-4o",
+  tags: ["gpt-plus"],
+  temperature: 0.8,
+});
 
 /**
  * チャット応答AI
- * @param req 
- * @returns 
+ * @param req
+ * @returns
  */
 export async function POST(req: Request) {
-  try{
+  try {
     const body = await req.json();
     const messages = body.messages ?? [];
 
+    // チャット形式
+    const formatMessage = (message: VercelChatMessage) => {
+      return `${message.role}: ${message.content}`;
+    };
+
     // 過去の履歴{chat_history}
-    const formattedPreviousMessages = messages
-      .slice(0, -1)
-      .map(formatMessage)
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
 
     // メッセージ{input}
     const currentMessageContent = messages[messages.length - 1].content;
 
-    // モデルの指定
-    const model = new ChatOpenAI({
-      apiKey: process.env.OPENAI_API_KEY!,
-      model: 'gpt-4o',
-      temperature: 0.8,
-      });
-        
-    const prompt = PromptTemplate.fromTemplate(PROMPT_TEMPLATE_TEXT);
+    const template = await client.pullPromptCommit("api-langchain");
+    const prompt = PromptTemplate.fromTemplate(
+      template.manifest.kwargs.template
+    );
     const chain = prompt.pipe(model);
 
     const stream = await chain.stream({
-      history: formattedPreviousMessages.join('\n'),
+      history: formattedPreviousMessages.join("\n"),
       input: currentMessageContent,
     });
 
@@ -47,13 +51,13 @@ export async function POST(req: Request) {
     if (error instanceof Error) {
       return new Response(JSON.stringify({ error: error.message }), {
         status: 500,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
       });
     }
- 
-    return new Response(
-      JSON.stringify({ error: 'Unknown error occurred' }),
-      { status: 500, headers: { 'Content-Type': 'application/json' } },
-    );
+
+    return new Response(JSON.stringify({ error: "Unknown error occurred" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
